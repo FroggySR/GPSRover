daa7e5ec4a10380a3806103aab3d9b8c7a5e2798
Søren Rasmussen <froggy_sr@hotmail.com>
2021-07-29 21:13:20 +0200

    Rover seems perfect... RCTM fails CRC

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,18 @@ const char *ssid = "UniFi";
 //const char *ssid = "SRiPhone";
 const char *password = "Logitech";
 
-//const IPAddress serverIP(192,168,0,200); //Address to visit
-const IPAddress serverIP(85, 191, 171, 18); //Address to visit External IP
-uint16_t serverPort = 200;                  //Server port number
+const IPAddress serverIP(192, 168, 0, 200); //Address to visit
+//const IPAddress serverIP(85, 191, 171, 18); //Address to visit External IP
+uint16_t serverPort = 200; //Server port number
 
 WiFiClient client; //Declare a client object to connect to the server
 
 String readString;
+char c;
+char sendBuffer[1024];
+int i;
+int indexSend = 2;
+int lengthRctm = 0;
 
 void setup()
 {
@@ -74,17 +79,64 @@ Serial.print((char)Serial1.read());
     if (client.connect(serverIP, serverPort)) //Try to access the target address
     {
         Serial.println("Visit successful");
-        delay(100);
+        delay(2000);
         //client.print("X");                    //Send data to the server
         while (client.connected() || client.available()) //If it is connected or has received unread data
         {
-
-
-
-        // Earlierer program  
+            //delay(2000);
+            //Serial.println("Visit successful");
+            //Serial.print("Connected: ");
+            //Serial.println(client.connected());
+            //Serial.print("Avalible: ");
+            //Serial.println(client.available());
+            // Earlierer program
             while (client.available()) //If there is data to read
             {
-                
+
+                sendBuffer[indexSend] = client.read();
+
+                if (sendBuffer[indexSend - 1] == 0xd3 && sendBuffer[indexSend] == 0x00 && indexSend != 1) // Find beginning of message
+                {
+                    sendBuffer[0] = 0xd3;
+                    sendBuffer[1] = 0x00;
+                    indexSend = 1;
+                }
+
+                lengthRctm = (int)sendBuffer[2] + 5; // Get lenght of RCTM message
+
+                if (indexSend > 1023 || indexSend < 0) // Overflow send buffer
+                {
+                    indexSend = 0;
+                    Serial.println("Overflow");
+                }
+
+                if (indexSend == lengthRctm && indexSend > 10 && lengthRctm > 10)
+                {
+                    Serial.print("lengthRctm: ");
+                    Serial.println(lengthRctm);
+
+                    if (client.connected() == true)
+                    {
+                        Serial1.write(sendBuffer, lengthRctm);
+                    }
+                    /*
+    for (int i = 0; i <= lengthRctm; i++)
+    {
+      if (client.connected() == true)
+      {
+        client.write(sendBuffer[i]);
+      }
+      //Serial.print(sendBuffer[i]);
+    }
+    */
+
+                    Serial1.flush();
+                    delay(10);
+                }
+                // serverBytesSent++;
+                //lastSentRTCM_ms = millis();
+                indexSend++;
+                /*
                 if (client.peek() == 0xD3)
                 {
                     delay(500);
@@ -117,17 +169,20 @@ Serial.print((char)Serial1.read());
                 
             }
             
+
+
             delay(5);
+            */
+            }
+            //Serial.println("Close the current connection");
+            //client.write("X");
+            //delay(900);
+            //client.stop(); //Close the client
         }
-        Serial.println("Close the current connection");
-        client.write("X");
-        //delay(900);
-        client.stop(); //Close the client
     }
     else
     {
         Serial.println("Access failed");
         client.stop(); //Close the client
     }
-    delay(2000);
 }
